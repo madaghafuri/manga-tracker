a17b03969eac1ac4701560262c8002cb631ecd56
Muhammad <mada.ghafuri@gmail.com>
2021-10-22 23:15:05 +0700

    testing mangadex api auth/login POST method

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Image, TouchableHighlight, FlatList, Pressable } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableHighlight, FlatList, Pressable, Button } from 'react-native';
+import Card from './components/SeriesCard';
+import NavTool from './components/Navigation';
 
 const mangaURL = "https://api.mangadex.org/manga/8f8b7cb0-7109-46e8-b12c-0448a6453dfa";
 const coverURL = "https://api.mangadex.org/cover/"
@@ -12,13 +14,12 @@ const App = () => {
   const [status, setStatus] = useState("Home");
 
   const displaySearch = () => {
-    fetch('https://api.mangadex.org/manga',{
-      method: 'GET',
-      'Content-Type': 'application/json'
+    fetch('https://api.mangadex.org/auth/login',{
+      method: 'POST'
     })
     .then(response => response.json())
     .then((json) => {
-      console.log(json.data[10])
+      console.log(json)
     })
     .catch(error => console.log(error));
   }
@@ -26,38 +27,22 @@ const App = () => {
   return (
     <View style={styles.container}>
       <Header header={status}/>
+      <Button title="authLogin" onPress={displaySearch}></Button>
       <Content />
       <NavBar toLibrary={() => setStatus("Library")} toHome={() => setStatus("Home")} toSearch={() => setStatus("Search")}/>
     </View>
   );
 }
 
-const NavBar = ({
-  
-}) => {
+const NavBar = (props) => {
     const [selected, setSelected] = useState('Home');
     const selectedColor = '#007AFF'
     
     return (
       <View style={styles.navBarContainer}>
-        <View style={styles.navBarIconContainer}>
-          <TouchableHighlight onPress={() => {setSelected('Library')}}>
-            <Image source={{uri: 'https://cdn.discordapp.com/attachments/776395062233923626/897737152727748608/unknown.png'}} style={selected == 'Library' ? styles.navBarIconSelected : styles.navBarIcon}></Image>
-          </TouchableHighlight>
-          <Text style={selected == 'Library' ? {color:selectedColor} : {color: '#a6a5ad'}}>Library</Text>
-        </View>
-        <View style={styles.navBarIconContainer}>
-          <TouchableHighlight onPress={() => {setSelected('Home')}}>
-            <Image source={{uri: 'https://cdn.discordapp.com/attachments/776395062233923626/897411587672014858/unknown.png'}} style={selected == 'Home' ? styles.navBarIconSelected : styles.navBarIcon}></Image>
-          </TouchableHighlight>
-          <Text style={selected == 'Home' ? {color:selectedColor} : {color: '#a6a5ad'}}>Home</Text>
-        </View>
-        <View style={styles.navBarIconContainer}>
-          <TouchableHighlight onPress={() => {setSelected('Search')}}>
-            <Image source={require('./assets/loupe.png')} style={selected == 'Search' ? styles.navBarIconSelected : styles.navBarIcon}></Image>
-          </TouchableHighlight>
-          <Text style={selected == 'Search' ? {color:selectedColor} : {color: '#a6a5ad'}}>Search</Text>
-        </View>
+        <NavTool onClick={() => setSelected('Library')} iconStyle={selected == 'Library' ? styles.navBarIconSelected : styles.navBarIcon} textStyle={selected == 'Library' ? {color: selectedColor} : {color: '#a6a5ad'}} iconText={"Library"} iconSource={'https://cdn.discordapp.com/attachments/776395062233923626/897737152727748608/unknown.png'}/>
+        <NavTool onClick={() => setSelected('Home')} iconStyle={selected == 'Home' ? styles.navBarIconSelected : styles.navBarIcon} textStyle={selected == 'Home' ? {color: selectedColor} : {color: '#a6a5ad'}} iconText={"Home"} iconSource={'https://cdn.discordapp.com/attachments/776395062233923626/897411587672014858/unknown.png'}/>
+        <NavTool onClick={() => setSelected('Search')} iconStyle={selected == 'Search' ? styles.navBarIconSelected : styles.navBarIcon} textStyle={selected == 'Search' ? {color: selectedColor} : {color: '#a6a5ad'}} iconText={"Search"} iconSource={'https://cdn.discordapp.com/attachments/776395062233923626/901135118566051930/loupe.png'}/>
       </View>
     )
   
@@ -79,45 +64,35 @@ const Content = (props) => {
   useEffect(() => {
     fetch(mangaURL,{
       method: 'GET',
-      'Content-Type': 'application/json'
+      headers: {
+        'Content-Type': 'application/json'
+      }
     })
     .then((response) => response.json())
     .then((json) => {
-      const coverId = json.data.relationships[2].id;
-      const titleId = json.data.id;
+      let coverId = json.data.relationships[2].id;
+      let titleId = json.data.id;
       setTitleId(titleId)
-      const seriesTitle = json.data.attributes.altTitles[0].en;
+      let seriesTitle = json.data.attributes.altTitles[0].en;
       setTitle(seriesTitle)
       console.log('coverid, titleid, & title obtained')
       return fetch(coverURL + coverId, {
         method: 'GET',
-        'Content-Type': 'application/json'
+        headers: {
+          'Content-Type': 'application/json'
+        }
       })
     })
     .then(response => response.json())
     .then((json) => {
-      const coverFile = json.data.attributes.fileName
+      let coverFile = json.data.attributes.fileName
       setCoverFile(coverFile)
       console.log('coverFile obtained')
     })
     .catch(error => console.log(error))
   }, [])
-
   return (
-    <View style={{
-      // alignItems: 'center',
-      padding: 10,
-      backgroundColor: 'black',
-    }}>
-      <Pressable >
-      <Image source={{uri: displayCoverURL + titleId + "/" + coverFile + ".512.jpg"}}    style={StyleSheet.create({
-          width: 180,
-          height: 270,
-          borderRadius: 5,
-        })}></Image>
-      </Pressable>
-      <Text style={styles.seriesTitle}>{title}</Text>
-    </View>
+      <Card coverLink={displayCoverURL + titleId + "/" + coverFile} seriesTitle={title}></Card>
   )
 }
 
